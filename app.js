bf0eea4effab553b8f9b002c5fda795f0a9b5ef3
Gaëtan BERNHARD <gaetan.bernhard@mailo.com>
2022-04-27 11:05:31 +0800

    [TD-3] Formulaire + routing IMC

--- app.js
+++ app.js
@@ -8,17 +8,29 @@ const header = require('./lib/Header');
 const nav = require('./lib/Nav');
 const footer = require('./lib/Footer');
 
-const accueil = require('./lib/Accueil');
-const calculIMC = require('./lib/CalculIMC');
 const convertisseurDevise = require('./lib/ConvertisseurDevise');
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.write(header.view);
-  res.write(nav.view);
-  res.write(accueil.view);
-  res.write(footer.view);
+  if(req.url === '/favicon.ico') {
+    res.statusCode = 204;
+
+  } else {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.write(header.view);
+    res.write(nav.view);
+
+    if (req.url === '/calculIMC') {
+      const calculIMC = require('./lib/CalculIMC');
+      res.write(calculIMC.getView);
+    } else {
+      const accueil = require('./lib/Accueil');
+      res.write(accueil.view);
+    }
+
+    res.write(footer.view);
+  }
+
   res.end();
 });
 
