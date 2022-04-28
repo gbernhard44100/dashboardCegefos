8332a107c2f28d0338c1553ca4df5c9d0a4db27f
Gaëtan BERNHARD <gaetan.bernhard@mailo.com>
2022-04-28 20:28:57 +0800

    [TD-3] Handle properly POST request

--- app.js
+++ app.js
@@ -1,37 +1,58 @@
 require('dotenv').config();
 const http = require('http');
+const { resolve } = require('path');
 
 const port = process.env.port;
 const hostname = process.env.hostname;
 
-const header = require('./lib/Header');
-const nav = require('./lib/Nav');
-const footer = require('./lib/Footer');
-
 const convertisseurDevise = require('./lib/ConvertisseurDevise');
 
-const server = http.createServer((req, res) => {
-  if(req.url === '/favicon.ico') {
-    res.statusCode = 204;
+async function handleHTTPRequest(req, res, body = '') {
+  var mainContent = '';
 
+  if (req.url === '/calculIMC') {
+    const calculIMC = require('./lib/CalculIMC').calculIMC;
+    var mainContent = await calculIMC.render(req);
   } else {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.write(header.view);
-    res.write(nav.view);
-
-    if (req.url === '/calculIMC') {
-      const calculIMC = require('./lib/CalculIMC');
-      res.write(calculIMC.getView);
-    } else {
-      const accueil = require('./lib/Accueil');
-      res.write(accueil.view);
-    }
-
-    res.write(footer.view);
+    const accueil = require('./lib/Accueil');
+    var mainContent = accueil.view;
   }
 
+  sendResponse(res, mainContent);
+}
+
+async function sendResponse(res, mainContent) {
+  const header = require('./lib/Header');
+  const nav = require('./lib/Nav');
+  const footer = require('./lib/Footer');
+
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.write(header.view);
+  res.write(nav.view);
+  res.write(mainContent);
+  res.write(footer.view);
   res.end();
+}
+
+const server = http.createServer((req, res) => {
+  if(req.url === '/favicon.png') {
+    res.statusCode = 204;
+    res.end();
+  } else {
+    var body = '';
+    
+    req.on('readable', function() {
+      if (req.method === 'GET') {
+        handleHTTPRequest(req, res);
+      } else if (req.method === 'POST') {
+        // http module handle the same request twice with no data to read in the second time.
+        while (null !== (body = req.read())) {
+          handleHTTPRequest(req, res, body);
+        }
+      }
+    });
+  }
 });
 
 server.listen(port, hostname, () => {
